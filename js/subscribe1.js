5961d22020a650af629596064b4adca214baacbd
KHW1025 <119498531+KHW1025@users.noreply.github.com>
2023-02-21 17:23:04 +0900

    ver0.22

--- js/subscribe1.js
+++ js/subscribe1.js
@@ -8,51 +8,67 @@ $ham.addEventListener("click", function () {
   $hd.classList.toggle("on");
 });
 
-// modal x_button
-const $product1 = document.querySelector(".item1");
-const $product2 = document.querySelector(".item2");
-const $product3 = document.querySelector(".item3");
-const $product4 = document.querySelector(".item4");
-const $product5 = document.querySelector(".item5");
-const $product6 = document.querySelector(".item6");
-const $product7 = document.querySelector(".item7");
-const $product8 = document.querySelector(".item8");
-const $product9 = document.querySelector(".item9");
-const $product10 = document.querySelector(".item10");
+// modal
+const $product = document.querySelectorAll(".product_con .product_item");
 const $modal = document.querySelector(".subscribe_modal");
-const $x = document.querySelector(".x_button");
+const $close = document.querySelector(".close");
 
-$product1.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product2.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product3.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product4.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product5.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product6.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product7.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product8.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product9.addEventListener("click", function () {
-  $modal.classList.add("on");
-});
-$product10.addEventListener("click", function () {
-  $modal.classList.add("on");
+// checklist list
+const list1 = document.querySelector(".list1"); //
+const list2 = document.querySelector(".list2"); //
+const list3 = document.querySelector(".list3"); //
+const list4 = document.querySelector(".list4"); //
+const list5 = document.querySelector(".list5"); //
+
+const LMonth = document.querySelectorAll(".month"); //
+const $month = document.querySelector(".product_month"); //
+
+const $penalty = document.querySelector(".penalty");
+const $penaltyPrice = document.querySelector(".penalty_price");
+
+const LPenalty = [6000, 6000, 11000, 15000, 6000, 6000, 15000, 15000];
+// 그린, 옐로싱글, 옐로더블, 옐로트리플, 블루, 크림슨, 바이올렛20, 바이올렛30
+// 패널티 금액을 변경하려면 이곳에서 변경
+
+const LPrice = document.querySelectorAll(".price"); //
+const LName = document.querySelectorAll(".level_name"); //
+const $price = document.querySelector(".product_price"); //
+const $name = document.querySelector(".product_name"); //
+
+$product.forEach((item, i) => {
+  item.addEventListener("click", function () {
+    $modal.classList.add("on");
+    $price.innerText = LPrice[i].innerText; // 상품 가격
+    $name.innerText = LName[i].innerText; // 상품이름
+    $month.innerText = LMonth[i].innerText; // 개월
+    $penaltyPrice.innerText = (
+      Number(LPrice[i].innerText) + LPenalty[i]
+    ).toLocaleString(); // 상품가격 + 패널티 금액
+    $penalty.innerText = LPenalty[i].toLocaleString(); // 패널티 금액
+
+    if (i == 0) {
+      list1.style.display = "list-item";
+      list2.style.display = "list-item";
+      list3.style.display = "list-item";
+      list4.style.display = "none";
+      list5.style.display = "none";
+    } else if (i > 0 && i < 8) {
+      list1.style.display = "none";
+      list2.style.display = "list-item";
+      list3.style.display = "list-item";
+      list4.style.display = "none";
+      list5.style.display = "none";
+    } else if (i > 7) {
+      list1.style.display = "none";
+      list2.style.display = "none";
+      list3.style.display = "none";
+      list4.style.display = "list-item";
+      list5.style.display = "list-item";
+    }
+  });
 });
 
-$x.addEventListener("click", function () {
+// close 버튼
+$close.addEventListener("click", function () {
   $modal.classList.remove("on");
 });
--- js/subscribe2.js
+++ js/subscribe2.js
@@ -9,107 +9,46 @@ $ham.addEventListener("click", function () {
 });
 
 // 설문 1
-const $red = document.querySelector(".red");
-const $red_white = document.querySelector(".red_white");
+const $type = document.querySelectorAll(".con1 > li");
 
-$red.addEventListener("click", function () {
-  if ($red.classList.contains("off") === false) {
-    $red.classList.add("off", "on");
-    $red_white.classList.add("off");
-  } else if ($red.classList.contains("on") === false) {
-    $red.classList.add("on");
-    $red_white.classList.remove("on");
-  }
-});
+$type.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    $type.forEach((a) => {
+      a.classList.remove("on");
+      a.classList.add("off");
+    });
 
-$red_white.addEventListener("click", function () {
-  if ($red_white.classList.contains("off") === false) {
-    $red_white.classList.add("off", "on");
-    $red.classList.add("off");
-  } else if ($red_white.classList.contains("on") === false) {
-    $red_white.classList.add("on");
-    $red.classList.remove("on");
-  }
+    $type[i].classList.remove("off");
+    $type[i].classList.add("on");
+  });
 });
 
 // 설문 2
-const $style1 = document.querySelector(".style1");
-const $style2 = document.querySelector(".style2");
-const $style3 = document.querySelector(".style3");
+const $style = document.querySelectorAll(".con2 > li");
 
-$style1.addEventListener("click", function () {
-  if ($style1.classList.contains("off") === false) {
-    $style1.classList.add("off", "on");
-    $style2.classList.add("off");
-    $style3.classList.add("off");
-  } else if ($style1.classList.contains("on") === false) {
-    $style1.classList.add("on");
-    $style2.classList.remove("on");
-    $style3.classList.remove("on");
-  }
-});
+$style.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    $style.forEach((a) => {
+      a.classList.remove("on");
+      a.classList.add("off");
+    });
 
-$style2.addEventListener("click", function () {
-  if ($style2.classList.contains("off") === false) {
-    $style2.classList.add("off", "on");
-    $style1.classList.add("off");
-    $style3.classList.add("off");
-  } else if ($style2.classList.contains("on") === false) {
-    $style2.classList.add("on");
-    $style1.classList.remove("on");
-    $style3.classList.remove("on");
-  }
-});
-
-$style3.addEventListener("click", function () {
-  if ($style3.classList.contains("off") === false) {
-    $style3.classList.add("off", "on");
-    $style1.classList.add("off");
-    $style2.classList.add("off");
-  } else if ($style3.classList.contains("on") === false) {
-    $style3.classList.add("on");
-    $style1.classList.remove("on");
-    $style2.classList.remove("on");
-  }
+    $style[i].classList.remove("off");
+    $style[i].classList.add("on");
+  });
 });
 
 // 설문 3
-const $sugar_c1 = document.querySelector(".sugar_c1");
-const $sugar_c2 = document.querySelector(".sugar_c2");
-const $sugar_c3 = document.querySelector(".sugar_c3");
+const $sugar = document.querySelectorAll(".con3 > li");
 
-$sugar_c1.addEventListener("click", function () {
-  if ($sugar_c1.classList.contains("off") === false) {
-    $sugar_c1.classList.add("off", "on");
-    $sugar_c2.classList.add("off");
-    $sugar_c3.classList.add("off");
-  } else if ($sugar_c1.classList.contains("on") === false) {
-    $sugar_c1.classList.add("on");
-    $sugar_c2.classList.remove("on");
-    $sugar_c3.classList.remove("on");
-  }
-});
+$sugar.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    $sugar.forEach((a) => {
+      a.classList.remove("on");
+      a.classList.add("off");
+    });
 
-$sugar_c2.addEventListener("click", function () {
-  if ($sugar_c2.classList.contains("off") === false) {
-    $sugar_c2.classList.add("off", "on");
-    $sugar_c1.classList.add("off");
-    $sugar_c3.classList.add("off");
-  } else if ($sugar_c2.classList.contains("on") === false) {
-    $sugar_c2.classList.add("on");
-    $sugar_c1.classList.remove("on");
-    $sugar_c3.classList.remove("on");
-  }
-});
-
-$sugar_c3.addEventListener("click", function () {
-  if ($sugar_c3.classList.contains("off") === false) {
-    $sugar_c3.classList.add("off", "on");
-    $sugar_c1.classList.add("off");
-    $sugar_c2.classList.add("off");
-  } else if ($sugar_c3.classList.contains("on") === false) {
-    $sugar_c3.classList.add("on");
-    $sugar_c1.classList.remove("on");
-    $sugar_c2.classList.remove("on");
-  }
+    $sugar[i].classList.remove("off");
+    $sugar[i].classList.add("on");
+  });
 });
